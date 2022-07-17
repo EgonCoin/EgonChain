f5391e3fccdfc79ab7118031711247c97dbe5f0f
topdefi <94203269+topdefi@users.noreply.github.com>
2022-07-16 23:42:22 -0700

    Update bootnodes

--- params/bootnodes.go
+++ params/bootnodes.go
@@ -22,14 +22,14 @@ import "github.com/ethereum/go-ethereum/common"
 // the main Ethereum network.
 var MainnetBootnodes = []string{
 	// Ethereum Foundation Go Bootnodes
-	"",
-	"",
+	"enode://d042cdd576e9d6b9deaf92b4fa5d3c40b2b194f28b2b830e14f68b684a7a4ea15bb4134e6619d9ec89bd50adb527bbb9feae713371169ce9590c214343efc54e@18.195.185.117:30303",
+	"enode://f61cfc11069955dc4f91f11bcd0985803f63e6d0d432326f3dda7de2981726a6facbb15f2193dbe549125c2b0a7011472016b86be81904428f714c3fbd32a679@35.170.32.132:30303",
 }
 
 // TestnetBootnodes are the enode URLs of the P2P bootstrap nodes running on the
 var TestnetBootnodes = []string{
-	"",
-	"",
+	"enode://c95f6f34a032f15d10125be9a30f2a5b3083a2db5ce8b33f23671bd4f75bb7587f70e91aecb0fc16283c699be22c4f89467be663607fcc582ab741ded29f64b0@3.123.66.255:30303",
+	"enode://551e1fd6c879790d9973c3f2e3f4be2dda46533227588a572713739e1eaa19ae952bc320a1923cfe4e6b7b3435c1f06d7a0c47b9ace0c44d831cd8809afda84f@34.199.236.89:30303",
 }
 
 var V5Bootnodes = []string{}
